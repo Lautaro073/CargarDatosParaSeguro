4696645a8c3a948a1e4bb04b7733108f6a65c18b
Lautaro073 <132080865+Lautaro073@users.noreply.github.com>
2024-03-01 12:43:35 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://falco.ezcell.repl.co/api/';
+axios.defaults.baseURL = 'https://e579d899-5765-441e-88e0-928df172bf65-00-3f02kychc5rgz.kirk.replit.dev/api/';
